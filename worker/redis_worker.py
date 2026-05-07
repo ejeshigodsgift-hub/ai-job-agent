c027b901c73f42b846faba3e3af81e16a133db73
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-07 11:07:41 +0100

    Create worker/redis_worker.py

---
+++ worker/redis_worker.py
@@ -0,0 +1,36 @@
+import json
+import time
+from queue.redis_client import r
+from worker.job_worker import process_job_search
+from worker.cv_worker import process_cv_generation
+
+
+def run_worker(worker_name="worker-1"):
+    print(f"{worker_name} started...")
+
+    while True:
+        task_data = r.brpop("task_queue", timeout=5)
+
+        if task_data is None:
+            continue
+
+        _, task_json = task_data
+        task = json.loads(task_json)
+
+        try:
+            print(f"{worker_name} processing {task['type']}")
+
+            if task["type"] == "job_search":
+                process_job_search(task)
+
+            elif task["type"] == "generate_docs":
+                process_cv_generation(task)
+
+        except Exception as e:
+            print("Worker error:", e)
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    run_worker()
