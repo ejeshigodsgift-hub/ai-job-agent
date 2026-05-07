4d590c4a04a371e6bf3ed9e2408b321cc844e177
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-07 13:02:01 +0100

    Create autonomous/rules.py

---
+++ autonomous/rules.py
@@ -0,0 +1,13 @@
+RULES = {
+    "free": {
+        "auto_apply": False
+    },
+    "pro": {
+        "auto_apply": False,
+        "email_drafts_only": True
+    },
+    "premium": {
+        "auto_apply": True,
+        "email_drafts_only": False
+    }
+}
