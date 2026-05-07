468d22e9c04d2f846f5cd03be72d73b56c942d27
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-07 07:55:58 +0100

    Create autopilot/autopilot_rules.py

---
+++ autopilot/autopilot_rules.py
@@ -0,0 +1,14 @@
+RULES = {
+    "free": {
+        "max_jobs": 3,
+        "auto_apply": False
+    },
+    "pro": {
+        "max_jobs": 10,
+        "auto_apply": False
+    },
+    "premium": {
+        "max_jobs": 20,
+        "auto_apply": True
+    }
+}
