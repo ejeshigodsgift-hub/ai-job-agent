0af2d942f37fc12a3ec15bed8bdff117cee4773f
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-09 20:02:43 +0100

    Update auth_routes.py

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from services.auth_service import create_user, login_user
+
+#from flask import Blueprint
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -9,3 +12,33 @@ def signup():
 @auth_bp.route("/signin")
 def signin():
     return {"message": "signin route"}
+
+
+auth_bp = Blueprint("auth", __name__)
+
+
+@auth_bp.route("/signup", methods=["POST"])
+def signup():
+    data = request.json
+
+    user = create_user(data)
+
+    return {
+        "message": "account created",
+        "user_id": user.id
+    }
+
+
+@auth_bp.route("/signin", methods=["POST"])
+def signin():
+    data = request.json
+
+    user = login_user(data["email"], data["password"])
+
+    if not user:
+        return {"error": "invalid credentials"}, 401
+
+    return {
+        "message": "signin successful",
+        "user_id": user.id
+    }
