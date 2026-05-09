be4276cacbb0f14df36f2d7cb024c7f72ec59183
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-09 20:05:22 +0100

    Update jobs_routes.py

--- routes/jobs_routes.py
+++ routes/jobs_routes.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+#from flask import Blueprint
+from flask import Blueprint, request
+from services.job_api_service import search_jobs
+
 
 jobs_bp = Blueprint("jobs", __name__)
 
 @jobs_bp.route("/jobs/search")
 def search_jobs():
     return {"message": "job search running"}
+
+
+jobs_bp = Blueprint("jobs", __name__)
+
+
+@jobs_bp.route("/jobs/search", methods=["POST"])
+def search_jobs_route():
+    data = request.json
+
+    jobs = search_jobs(data["skill"])
+
+    return jobs
