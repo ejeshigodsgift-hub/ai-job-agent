07be15ff4996760387a2b0bd73b1eb2d47673afc
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-09 19:40:18 +0100

    Create notification_service.py

---
+++ services/notification_service.py
@@ -0,0 +1,10 @@
+def send_telegram(user, message):
+    print("telegram notification sent")
+
+
+def send_whatsapp(user, message):
+    print("whatsapp notification sent")
+
+
+def send_email(user, message):
+    print("email notification sent")
