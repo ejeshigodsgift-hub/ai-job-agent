9a527c41fecf9febf4c567920006b4168e8e5c6b
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-10 15:12:36 +0100

    Delete password_utils.p

--- auth/password_utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
-
-
-def verify_password(password, hashed):
-    return hash_password(password) == hashed
