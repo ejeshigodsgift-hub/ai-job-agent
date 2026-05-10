11c71647c5c5aacdb945a3ba722f84fdbee672d2
ejeshigodsgift-hub <ejeshigodsgift@gmail.com>
2026-05-10 15:14:50 +0100

    Delete events.py

--- analytics/events.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def track_event(user_id, event_name):
-    print({
-        "user": user_id,
-        "event": event_name,
-        "time": str(datetime.utcnow())
-    })
